8cf2a794cf465992c0b0071314b4097763f117b7
alfred-jayaprakash <alfred.jayaprakash@gmail.com>
2021-01-27 20:58:20 +0800

    Added puppet solutions

---
+++ puppet/kke-setup-ssh-keys.pp
@@ -0,0 +1,41 @@
+$public_key =  'AAAAB3NzaC1yc2EAAAADAQABAAABAQCjzucWviIHiT0R1YP3cYmkWcNfv53svphAIW4RpnDiSdoTvooeah3Akh/VagwCJsClpdwuM3xdAvEWyHFkI6zdItrdjqM8fJ6Y8HYXF8Ros979TVYcktI8Ird+92CFqsAVRqGTyJNx++68N7JA78dWf+SEsGaDSkEjGkjfIJgOlZ1OmJJB/pszUOjeiFvEJbkc+TA0fH6htGg/QCotC1tAUnIszf664QENjNiqIfruM/CwojExmos8RKKO1GYgjBFzB9eofk7zsjn1zk9NJ7LGqvZ6/EirTf2dCOH5RMYbjccGZI/AQTXQ15kUYUHCtpUQFrQ88T0W93D9bbiXHdFn'
+
+class ssh_node1 {
+  ssh_authorized_key { 'tony@stapp01':
+    ensure => present,
+    user   => 'tony',
+    type   => 'ssh-rsa',
+    key    => $public_key,
+  }
+}
+
+class ssh_node2 {
+  ssh_authorized_key { 'steve@stapp02':
+    ensure => present,
+    user   => 'steve',
+    type   => 'ssh-rsa',
+    key    => $public_key,
+  }
+}
+
+class ssh_node3 {
+  ssh_authorized_key { 'banner@stapp03':
+    ensure => present,
+    user   => 'banner',
+    type   => 'ssh-rsa',
+    key    => $public_key,
+  }
+}
+
+node stapp01.stratos.xfusioncorp.com {
+  include ssh_node1
+}
+
+node stapp02.stratos.xfusioncorp.com {
+  include ssh_node2
+}
+
+node stapp03.stratos.xfusioncorp.com {
+  include ssh_node3
+}
+
