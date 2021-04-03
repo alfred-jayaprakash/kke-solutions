b766f77c57b4a8bef135080f7ccb85684b1eed22
alfred-jayaprakash <alfred.jayaprakash@gmail.com>
2021-04-03 08:11:46 +0800

    Puppet Yum localrepo solution

--- puppet/kke-local-yum-repo.pp
+++ puppet/kke-local-yum-repo.pp
@@ -1,26 +1,30 @@
 #
 # Step 1: Save this file under /etc/puppetlabs/code/environments/production/manifests 
 #         as a file with the name specified in the question e.g. news.pp
-# Step 2: Run the puppet verifications steps mentioned in puppet/README.md
-# Step 3: Finally, SSH to each hosts and run `sudo puppet agent -tv` to make
+# Step 2: Change the yum repo name, paths and application package according to question
+# Step 3: Run the puppet verifications steps mentioned in puppet/README.md
+# Step 4: Finally, SSH to each hosts and run `sudo puppet agent -tv` to make
 #         sure you install the package
-# Step 4: Verify: Check that the required package e.g. samba is installed from local 
-#         repo using 'repoquery -i samba'
-#         [(]You may have to install `yum-utils` for repoquery to work - 
-#          'sudo yum install -y yum-utils']
+# Step 5: Verify: Check that the required package (e.g. httpd) is installed from local 
+#         repo using 'repoquery -i httpd'
+#         ('Repository' field should show as 'local_yum')
+#         Note: You may have to install `yum-utils` for repoquery to work - 
+#          'sudo yum install -y yum-utils'
 #
 # For tips on getting better at Puppet tasks, check out the README.md
 # in this folder
 #
 class local_yum_repo {
-  yumrepo { ‘localyum’:
+  #Setup Local repo
+  yumrepo { 'localyum':
     enabled => 1,
-    descr => ‘Local repo for application packages’,
-    baseurl => ‘file:///packages/downloaded_rpms’,
+    descr => 'Local repo for app pckgs',
+    baseurl => 'file:///packages/downloaded_rpms',
     gpgcheck => 0,
   }
 
-  package { 'samba':
+  #Install package from this repo
+  package { 'httpd':
     ensure  => 'installed',
     require => Yumrepo['localyum'],        
   }
